c98869678927633ae0e3e37bea58cc777efd06cb
Samanja Cartagena <c.samanja09@Pabs.local>
2024-10-09 18:22:00 -0400

    first commit

---
+++ datapulling/realtor.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 16:30:44 2024
+
+@author: c.samanja09
+"""
+
+import csv
+
+with open('C:\\Users\\c.samanja09\\Desktop\\numbers.csv','r', encoding='ANSI') as file:
+    print(file.read())
