1639bbc76a65818c9b82d0dce631d85dafb200fe
Samanja Cartagena <c.samanja09@Pabs.local>
2024-10-12 01:37:57 -0400

    data pulled

--- datapulling/realtor.py
+++ datapulling/realtor.py
@@ -19,46 +19,71 @@ from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup as bs
 import datetime
 
-'''
+
 driver = webdriver.Chrome()
 
-driver.get("https://www.youtube.com/")
-time.sleep(3)
-
-driver.maximize_window()
-
-search=driver.find_elements(By.TAG_NAME,"input")
-
-search[0].click()
-
-search[0].send_keys('Kevin Ray Ward')
-
-btn=driver.find_element(By.XPATH,"//button[@aria-label='Search']").click()
-time.sleep(10)
 
 
 
-found=driver.find_elements(By.XPATH,"//*[contains(text(), 'Kevin Ray Ward')]")
 
-realtor=driver.find_elements(By.XPATH,"//*[contains(text(), 'real estate')]")
-
-if len(found)>5 and len(realtor)>5:
-    print('Daniel Kosasih exists')
-    print(len(found))
-    print(len(realtor))
-    
-else:
-    print('No')
-
-'''
+my_list=[]
 with open('C:\\Users\\c.samanja09\\Desktop\\numbers.csv','r', encoding='ANSI') as file:
        reader = csv.DictReader(file)
        columnNames = reader.fieldnames
        for row in reader:
-        print(row['First Name']+" "+row['Last Name'])
-       with open('names.csv',mode='w') as new_file:
-            fieldnames=['First Name','Last Name']
-            writer=csv.DictWriter(new_file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow({"First Name":row['First Name'],"Last Name":row['Last Name']})
-     
+          driver.get("https://www.youtube.com/")
+          time.sleep(3)
+
+          driver.maximize_window()
+
+          search=driver.find_elements(By.TAG_NAME,"input")
+
+          search[0].click()
+          full_name=row['First Name']+" "+row['Last Name']
+          info=row['First Name']+","+row['Last Name']+","+row['Email Address']+","+row['Company Name']+","+row['Phone Number']
+          search[0].send_keys(full_name)
+
+          btn=driver.find_element(By.XPATH,"//button[@aria-label='Search']").click()
+          time.sleep(10)
+
+          found=driver.find_elements(By.XPATH,'//*[contains(text(), "' + full_name + '")]')
+          
+          realtor=driver.find_elements(By.XPATH,"//*[contains(text(), 'real estate')]")
+          time.sleep(5)
+          
+          if len(found)>5 and len(realtor)>5:
+              print(full_name+'YouTube account exists')
+              print(len(found))
+              print(len(realtor))
+              new_file=open('youtube.csv','a',newline='')
+              writer=csv.writer(new_file)
+              writer.writerows(info)
+              
+          else:
+              print('No')
+
+         
+    
+
+
+
+
+
+'''
+
+
+d=['Sam',"Cartagena",'cart.sam@gmail.com']
+
+
+
+data_append=[
+    ['Janet','Canada','janet@yahoo.com'],
+    ['Pierre','France','pierre@gmail.com']
+    ]
+data_append.append(d)
+file=open('name.csv','a',newline='')
+writer=csv.writer(file)
+writer.writerows(data_append)
+file.close()
+
+'''
